991ef0d65733b37ac7ba143e4aa757ddfa2080c5
PasztorGergo <pasztorg05@gmail.com>
2024-03-22 20:28:39 +0100

    streaming-data

--- server.py
+++ server.py
@@ -2,6 +2,10 @@ from flask import Flask, request,render_template
 #from face_anim import set_emotion
 
 app = Flask(__name__)
+global hs
+global emotion
+hs = True
+emotion = 0
 
 @app.route("/")
 def index():
@@ -9,8 +13,13 @@ def index():
     return render_template("index.html")
 
 @app.route("/emotion", methods=["POST"])
+
 def setEmtoion():
     id = int(request.get_json()["id"])
+    emotion = id
+    fp = open("db.txt", "w")
+    fp.write(f"{emotion}\t{hs}")
+    fp.close()
     #set_emotion(id)
     return ("", 204)
 
@@ -19,5 +28,18 @@ def sendStatus():
     print("Received")
     return {"version":"1.0"}
 
+@app.route("/hall-effect", methods=["POST"])
+def setMotuhSync():
+    hs = request.get_json()["state"]
+    fp = open("db.txt", "w")
+    fp.write(f"{emotion}\t{hs}")
+    fp.close()
+    print("Mouth sync toggled")
+    return("", 204)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=3000, debug=True)
+    fp = open("db.txt", "w")
+    fp.write(f"{0}\t{True}")
+    fp.close()
---
+++ tests.py
@@ -0,0 +1,31 @@
+import time
+
+class Animation:
+    mouth_sync_ena = True
+    emotions = ("Default","Happy","Tired","Eyes-closed","Angry","Furious","Confiused","Thinking","Inquestitive","Hungary")
+    emotion_id = 0
+    data = []
+
+    def __init__(self):
+        print("Initializer functions: ON")
+
+    def loop(self):
+        fp = open("db.txt")
+        splited = fp.readline().split("\t")
+        self.emotion_id = int(splited[0])
+        self.mouth_sync_ena = bool(splited[1])
+
+        if self.mouth_sync_ena:
+            print("Lip sync enabled")
+        else:
+            print("Lip sync disabled")
+        time.sleep(2)
+        print("Blink")
+        print("Emotion:", self.emotions[self.emotion_id])
+
+
+ani = Animation()
+
+if __name__ == "__main__":
+    while True:
+        ani.loop()
