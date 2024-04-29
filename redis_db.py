18cb6a225e707424d43641458fa97b24748d197a
PasztorGergo <pasztorg05@gmail.com>
2024-04-29 12:47:23 +0200

    reading-redis

---
+++ redis_db.py
@@ -0,0 +1,11 @@
+import time
+import requests
+import json
+
+if __name__ == "__main__":
+    while True:
+        time.sleep(0.025)
+        data = requests.get("https://proto-web-panel.vercel.app/api/synchronize").json()
+        with open("db.txt", "w") as fs:
+            fs.write(f"{data["id"]}\t{data["mouth"]}\t{data["patriotism"]}\t1")
+            fs.close()
