84888ca676223cd1c562c3ab4e0f199bf8372798
PasztorGergo <pasztorg05@gmail.com>
2024-05-02 10:26:47 +0200

    eyes-redesign

--- redis_db.py
+++ redis_db.py
@@ -7,8 +7,9 @@ if __name__ == "__main__":
         time.sleep(0.025)
         data = requests.get("https://proto-web-panel.vercel.app/api/synchronize").json()
         try:
-            with open("/db.txt", "w") as fs:
-                fs.write(f'{data["id"]}\t{data["mouth"]}\t{data["patriotism"]}\t1')
-                fs.close()
+            if data["id"]:
+                with open("/db.txt", "w") as fs:
+                    fs.write(f'{data["id"]}\t{data["mouth"]}\t{data["patriotism"]}\t1')
+                    fs.close()
         except:
             pass
