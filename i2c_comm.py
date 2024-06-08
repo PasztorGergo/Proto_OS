a3cf5ecaf01969da00241a2795ae6142cf81ad58
PasztorGergo <pasztorg05@gmail.com>
2024-06-08 11:46:12 +0200

    i2c-comm-fix

--- i2c_comm.py
+++ i2c_comm.py
@@ -1,4 +1,5 @@
 import smbus2
+from typing import Literal
 
 DEVICE_BUS = 1
 DEVICE_ADDR = 0x15
@@ -6,7 +7,6 @@ DEVICE_ADDR = 0x15
 bus = smbus2.SMBus(DEVICE_BUS)
 
 def send_emotion(id):
-    hex_values = [0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09]
-    bus.write_byte_data(DEVICE_ADDR, 0x00, hex_values[id])
+    bus.write_byte_data(DEVICE_ADDR, 0x00, id)
 
 
