159885a47545753220e774aecd28d3e462cd5917
PasztorGergo <pasztorg05@gmail.com>
2024-07-14 20:00:33 +0200

    i2c_register_addresses

--- i2c_comm.py
+++ i2c_comm.py
@@ -9,14 +9,14 @@ def send_static_emotion(id):
 
 def send_dynamic_emotion(eye,id):
     with SMBus(DEVICE_BUS) as bus:
-        bus.write_block_data(TEENSY_ADDR, 0x00, [0b01,eye,id])
+        bus.write_block_data(TEENSY_ADDR, 0x01, [0b01,eye,id])
 
 def send_secondary_feature(state,feature):
     with SMBus(DEVICE_BUS) as bus:
-        bus.write_block_data(TEENSY_ADDR, 0x00, [0b10,state,feature])
+        bus.write_block_data(TEENSY_ADDR, 0x02, [0b10,state,feature])
 
 def send_primary_feature(state,feature):
     with SMBus(DEVICE_BUS) as bus:
-        bus.write_block_data(TEENSY_ADDR, 0x00, [0b11,state,feature])
+        bus.write_block_data(TEENSY_ADDR, 0x03, [0b11,state,feature])
 
 
