f47a8489137a8fe61ff5b01726278a1276d397fb
PasztorGergo <pasztorg05@gmail.com>
2024-07-14 19:47:08 +0200

    uart_crash_protection

--- uart_connection.py
+++ uart_connection.py
@@ -1,4 +1,5 @@
 import serial
+import time
 from i2c_comm import send_dynamic_emotion
 # Initialize serial connection
 ser = serial.Serial('/dev/serial0', 9600, timeout=1)
@@ -9,4 +10,7 @@ while True:
         splitted = fs.readline().replace("\n", "").split()
         if data and splitted[3]:
             eye_state = int(data)
-            send_dynamic_emotion(eye_state, splitted[0])
+            try:
+                send_dynamic_emotion(eye_state, int(splitted[0]))
+            except:
+                time.sleep(2.5)
